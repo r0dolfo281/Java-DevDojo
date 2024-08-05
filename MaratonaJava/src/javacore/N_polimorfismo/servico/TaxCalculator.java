dcde6352ed67f8654830df3bfb0f063fb101ccd9
RodolfoCF <rodolfo.cunhasque@gmail.com>
2024-08-05 00:06:20 -0300

    RCF
    Signed-off-by: RodolfoCF <rodolfo.cunhasque@gmail.com>

--- MaratonaJava/src/javacore/N_polimorfismo/servico/TaxCalculator.java
+++ MaratonaJava/src/javacore/N_polimorfismo/servico/TaxCalculator.java
@@ -1,6 +1,7 @@
 package javacore.N_polimorfismo.servico;
 
 import javacore.N_polimorfismo.dominio.DesktopComputer;
+import javacore.N_polimorfismo.dominio.Tomato;
 
 public class TaxCalculator {
 	public void taxDesktopComputerCalculator(DesktopComputer computer) {
@@ -8,8 +9,14 @@ public class TaxCalculator {
 		double tax = computer.taxCalculator();
 		System.out.printf("Product's name: %s", computer.getName());
 		System.out.printf("DesktopComputer's value: %d", computer.getValue());
-		System.out.printf("DesktopComputer's tax: %d", tax);
-		
+		System.out.printf("DesktopComputer's tax: %d%n", tax);
+	}
+	public void taxDesktopComputerCalculator(Tomato tomato) {
+		System.out.println("DesktopComputer's tax report");
+		double tax = tomato.taxCalculator();
+		System.out.printf("Product's name: %s", tomato.getName());
+		System.out.printf("Tomato value: %d", tomato.getValue());
+		System.out.printf("Toamto tax: %d", tax);
 	}
 
 }
